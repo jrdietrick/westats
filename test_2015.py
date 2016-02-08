8747c39ba422076be453c15a2a9401151a9888c6
John R Dietrick <john@johndietrick.com>
2016-02-08 21:13:06 +0800

    Hooked up to highcharts

--- test_2015.py
+++ test_2015.py
@@ -1,7 +1,9 @@
 import datetime
+import json
 import re
-
 from collections import defaultdict
+from dateutil.relativedelta import relativedelta
+
 from wxparser import Parser, UserData, Category, _slugify
 
 
@@ -11,27 +13,212 @@ class CST(datetime.tzinfo):
         return datetime.timedelta(hours=8)
 
     def tzname(self, dt):
-        return 'China Standard Time'
+        return 'Beijing Time'
 
     def dst(self, dt):
         return datetime.timedelta(hours=8)
 
 
 beijing_time = CST()
+beginning_of_2015 = datetime.datetime(2015, 1, 1, 0, 0, 0, 0, beijing_time)
+beginning_of_2016 = datetime.datetime(2016, 1, 1, 0, 0, 0, 0, beijing_time)
+beginning_of_2017 = datetime.datetime(2017, 1, 1, 0, 0, 0, 0, beijing_time)
+
+
+def build_sent_by_category_by_month_graph(wxp):
+    # Build the timespans
+    timespans = []
+    rolling_date = datetime.datetime(2015, 1, 1, 0, 0, 0, 0, beijing_time)
+    while rolling_date < beginning_of_2016:
+        next_start = rolling_date + relativedelta(months=1)
+        timespans.append((rolling_date, next_start))
+        rolling_date = next_start
+
+    raw_data = defaultdict(lambda: [0] * len(timespans))
+    for thread in wxp.individual_threads:
+        category_slug = thread.category.slug if getattr(thread, 'category', None) else 'other'
+        for i in xrange(0, len(timespans)):
+            from_timestamp, to_timestamp = timespans[i]
+            raw_data[category_slug][i] += thread.message_sent_count_between(from_timestamp, to_timestamp)
+
+    for thread in wxp.group_threads:
+        for i in xrange(0, len(timespans)):
+            from_timestamp, to_timestamp = timespans[i]
+            raw_data['group-chats'][i] += thread.message_sent_count_between(from_timestamp, to_timestamp)
+
+    sorted_keys = list(reversed(sorted(raw_data.keys(), key=lambda slug: sum(raw_data[slug]))))
+
+    series_data = []
+    for series in sorted_keys:
+        series_data.append({
+            'name': series,
+            'data': raw_data[series],
+        })
+
+    return {
+        'chart': {
+            'type': 'area'
+        },
+        'title': {
+            'text': 'Messages sent'
+        },
+        'subtitle': {
+            'text': 'per month, by category'
+        },
+        'colors': ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'],
+        'xAxis': {
+            'categories': [timespan[0].strftime('%Y-%m') for timespan in timespans],
+            'tickmarkPlacement': 'on',
+            'title': {
+                'enabled': False,
+            },
+        },
+        'yAxis': {
+            'title': {
+                'text': 'Messages sent'
+            },
+        },
+        'tooltip': {
+            'shared': True,
+            'valueSuffix': ' messages'
+        },
+        'plotOptions': {
+            'area': {
+                'stacking': 'normal',
+                'lineWidth': 1,
+            }
+        },
+        'series': series_data,
+    }
+
+
+def build_all_messages_scatterplot(wxp):
+
+    def _day_of_year(timestamp):
+        return int((timestamp - beginning_of_2015).total_seconds() / (60 * 60 * 24))
+
+    def _hour_of_day(timestamp):
+        return round(timestamp.hour + (timestamp.minute / 60.0), 2)
+
+    messages_sent = []
+    for thread in wxp.threads:
+        for message in thread.messages:
+            if message.timestamp < beginning_of_2015 or message.timestamp >= beginning_of_2016:
+                continue
+            if not message.sent:
+                continue
+            messages_sent.append([_day_of_year(message.timestamp.astimezone(beijing_time)), _hour_of_day(message.timestamp.astimezone(beijing_time))])
+
+    messages_received = []
+    for thread in wxp.individual_threads:
+        for message in thread.messages:
+            if message.timestamp < beginning_of_2015 or message.timestamp >= beginning_of_2016:
+                continue
+            if not message.sent:
+                continue
+            messages_received.append([_day_of_year(message.timestamp.astimezone(beijing_time)), _hour_of_day(message.timestamp.astimezone(beijing_time))])
+
+    return {
+        'chart': {
+            'type': 'scatter',
+            'zoomType': 'xy',
+        },
+        'colors': ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999'],
+        'title': {
+            'text': '2015 - All Messages',
+        },
+        'xAxis': {
+            'title': {
+                'enabled': True,
+                'text': 'Day of Year',
+            },
+            'tickPositions': [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365],
+            # 'startOnTick': True,
+            # 'endOnTick': True,
+            # 'showLastLabel': True,
+        },
+        'yAxis': {
+            'title': {
+                'text': 'Hour of Day (China Standard Time)',
+            },
+            'min': 0,
+            'max': 24,
+            'tickPositions': [0, 4, 8, 12, 16, 20, 24],
+        },
+        'plotOptions': {
+            'scatter': {
+                'marker': {
+                    'radius': 3,
+                },
+            },
+        },
+        'series': [
+            {
+                'name': 'Messages sent',
+                'color': 'rgba(55, 126, 184, .6)',
+                'data': messages_sent,
+            },
+            {
+                'name': 'Messages received',
+                'color': 'rgba(77, 175, 74, .6)',
+                'data': messages_received,
+            },
+        ]
+    }
+
+
+def render_highcharts(highchart_data):
+    chart_div_template = '''\
+    <div id="chart%(index)d" style="width: 1280px; height: 720px; margin-left: auto; margin-right: auto;"></div>
+'''
+
+    chart_divs = [chart_div_template % {'index': i} for i in xrange(0, len(highchart_data))]
+
+    chart_data_template = '''\
+        $(function () {
+            $('#chart%(index)d').highcharts(%(chart_data)s);
+        });
+'''
+
+    chart_data = [chart_data_template % {'index': i, 'chart_data': json.dumps(highchart_data[i])} for i in xrange(0, len(highchart_data))]
+
+    template = '''\
+<html>
+<head>
+</head>
+<body>
+%(chart_divs)s
+    <script type="text/javascript" src="x.jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="x.highcharts.js"></script>
+    <script type="text/javascript" src="x.highcharts.darktheme.js"></script>
+    <script type='text/javascript'>
+    // <![CDATA[
+%(chart_data)s
+    // ]]>
+    </script>
+</body>
+'''
+    return template % {
+        'chart_divs': ''.join(chart_divs),
+        'chart_data': ''.join(chart_data),
+    }
 
 
 if __name__ == '__main__':
     wxp = Parser('decrypted.db')
     userdata = UserData.initialize(wxp)
 
-    beginning_of_2015 = datetime.datetime(2015, 2, 19, 0, 0, 0, 0, beijing_time)
-    beginning_of_2016 = datetime.datetime(2016, 2, 8, 0, 0, 0, 0, beijing_time)
-
     def _chats_in_2015(thread):
         return thread.message_count_between(beginning_of_2015, beginning_of_2016)
 
     def _sent_chats_in_2015(thread):
         return thread.message_sent_count_between(beginning_of_2015, beginning_of_2016)
+
+    def _chats_in_2016(thread):
+        return thread.message_count_between(beginning_of_2016, beginning_of_2017)
+
+    def _sent_chats_in_2016(thread):
+        return thread.message_sent_count_between(beginning_of_2016, beginning_of_2017)
 
     total_individual_chats = sum([_chats_in_2015(thread) for thread in wxp.individual_threads])
     individual_sent_messages = sum([thread.message_sent_count_between(beginning_of_2015, beginning_of_2016) for thread in wxp.individual_threads])
@@ -81,6 +268,21 @@ if __name__ == '__main__':
         userdata.add_category(new_category)
         new_category.add_thread(thread)
         userdata.save()
+
+    print render_highcharts([
+        build_sent_by_category_by_month_graph(wxp),
+        build_all_messages_scatterplot(wxp),
+    ])
+
+    print
+    threads_by_sent = list(reversed(sorted(wxp.individual_threads, key=lambda thread: _sent_chats_in_2015(thread))))
+    for i in xrange(0, 10):
+        print threads_by_sent[i].contact.display_name, _sent_chats_in_2015(threads_by_sent[i]), _chats_in_2015(threads_by_sent[i]) - _sent_chats_in_2015(threads_by_sent[i])
+
+    print
+    threads_by_sent = list(reversed(sorted(wxp.individual_threads, key=lambda thread: _sent_chats_in_2016(thread))))
+    for i in xrange(0, 10):
+        print threads_by_sent[i].contact.display_name, _sent_chats_in_2016(threads_by_sent[i]), _chats_in_2016(threads_by_sent[i]) - _sent_chats_in_2016(threads_by_sent[i])
 
     by_category = defaultdict(lambda: 0)
     for thread in wxp.individual_threads:
