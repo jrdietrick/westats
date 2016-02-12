3863feeceaf6bf84eea0bb36e1db6e5eb3a07919
John R Dietrick <john@johndietrick.com>
2016-02-12 16:28:49 +0800

    Set default export size

--- renderers/highchart.py
+++ renderers/highchart.py
@@ -5,7 +5,13 @@ from renderers import template_env
 class HighchartRenderer(object):
 
     def __init__(self, highchart_data):
-        self.highchart_data = highchart_data
+        self.highchart_data = {
+            'exporting': {
+                'sourceWidth': 1280,
+                'sourceHeight': 800,
+            },
+        }
+        self.highchart_data.update(highchart_data)
 
     def render(self):
         return template_env.get_template('highchart.haml').render({'chart_json': json.dumps(self.highchart_data)})
